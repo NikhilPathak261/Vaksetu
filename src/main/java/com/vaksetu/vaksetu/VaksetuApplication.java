41113cf468c8ca71f38c60b4956adcd48fabee98
NikhilPathak261 <nikhilpathak261@gmail.com>
2026-03-12 23:08:00 +0530

    Initial Spring Boot Project Setup

---
+++ src/main/java/com/vaksetu/vaksetu/VaksetuApplication.java
@@ -0,0 +1,13 @@
+package com.vaksetu.vaksetu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaksetuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaksetuApplication.class, args);
+	}
+
+}
