768da4a4140790664f79e56e198f2c1101a2fc91
NikhilPathak261 <nikhilpathak261@gmail.com>
2026-03-12 23:46:03 +0530

    Add SessionService for practice session logic

--- src/main/java/com/vaksetu/vaksetu/service/SessionService.java
+++ src/main/java/com/vaksetu/vaksetu/service/SessionService.java
@@ -1,4 +1,23 @@
 package com.vaksetu.vaksetu.service;
 
+import com.vaksetu.vaksetu.model.Session;
+import com.vaksetu.vaksetu.repository.SessionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class SessionService {
+
+    @Autowired
+    private SessionRepository sessionRepository;
+
+    public Session createSession(Session session){
+        return sessionRepository.save(session);
+    }
+
+    public List<Session> getSessionsByUserId(Long userId){
+        return sessionRepository.findByUserId(userId);
+    }
 }
